5274b231bd779dd8b3d02dc806a162a4e4e7aa00
mamokmamok <mamokmamok@gmail.com>
2017-04-15 22:17:20 +0300

    fix the site

--- db/shortcuts.js
+++ db/shortcuts.js
@@ -1,19 +1,19 @@
 var utils = require('./utils');
 
 // Get all shortcuts from the utils library
-exports.getAllShortcuts = function (softwareId, cb) {
-    utils.findByFiled('shortcuts', 'software_id', parseInt(softwareId), cb);
+exports.getAllShortcuts = function (software_id, cb) {
+    utils.findByFiled('shortcuts', 'software_id', parseInt(software_id), cb);
 }
 
-exports.getAllCategories = function (softwareId, cb) {
-    utils.findByFiled('categories', 'software_id', parseInt(softwareId), cb)
-}
+exports.getAllCategories = function (software_id, cb) {
+    utils.findByFiled('categories', 'software_id', parseInt(software_id), cb);
+};
 
 exports.addShortcut = function (shortcut, cb) {
     utils.add('shortcuts', null, shortcut, cb);
-}
+};
 
 
 exports.deleteShortcut = function (id, cb) {
     utils.delete('shortcuts', id, cb);
-}
+};
